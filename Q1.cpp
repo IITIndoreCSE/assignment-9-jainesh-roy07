7d15b4570638a5c4cb812a69783d5436a38b1231
jainesh-roy07 <mems250005019@iiti.ac.in>
2025-10-17 16:13:27 +0530

    Update Q1.cpp

--- Q1.cpp
+++ Q1.cpp
@@ -1,1 +1,22 @@
 
+#include <iostream>
+using namespace std;
+
+
+struct Item{
+    int id;
+    float cost;
+};
+int main(){
+    Item items[6] = {{1, 32}, {2, 56}, {3, 56.87}, {4, 9.45}};
+
+    cout<<"Item id and Cost whose cost is greater than 50.00"<<endl;
+    for(int i = 0; i<6; i++){
+        if(items[i].cost>50){
+            cout<<items[i].id<<" : "<<items[i].cost<<endl;
+        }
+    }
+
+    
+    return 0; 
+}
